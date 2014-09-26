b07bfa302d4832f2c1cbbe84916325f83ce0ac43
Rodolfo Barreto <rodolfobarretoweb@gmail.com>
2014-09-26 13:15:34 -0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  var gruntConfig = {
+    pkg: grunt.file.readJSON('package.json'),
+
+    cssmin: {
+      combine: {
+        files: {
+          'css-print.min.css': ['css/reset.css']
+        }
+      },
+
+      minify : {
+        files : [{
+          expand : true,
+          cwd : '/',
+          src : ['css-print.min.css'],
+          dest : 'src',
+          ext : '.min.css'
+        }]
+      }
+    }
+  };
+
+  grunt.initConfig(gruntConfig);
+
+  // load plugins
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // tasks
+  grunt.registerTask('default', ['cssmin']);
+};
